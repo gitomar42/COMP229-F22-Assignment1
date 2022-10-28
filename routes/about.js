84c9988abb004e9a1cd01b32b3d67465fb85226b
Omar Ahmed <omarahmed@Omars-MBP.ht.home>
2022-10-28 06:20:35 +0300

    try about

--- routes/about.js
+++ routes/about.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET About Us page. */
 router.get('/', function(req, res, next) {
-    res.render('about', { title: 'About'});
+    res.render('index', { title: 'About'});
   });
-
+  
 module.exports = router;
