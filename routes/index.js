676a07cb4c1bf28491adcef4a660f6756b7d2882
Omar Ahmed <omarahmed@Omars-MBP.ht.home>
2022-10-28 06:23:04 +0300

    apply about

--- routes/index.js
+++ routes/index.js
@@ -11,6 +11,10 @@ router.get('/home', function(req, res, next) {
   res.render('index', { title: 'Home'});
 });
 
+/* GET About Us page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'About'});
+});
 
 
 /* GET Products page. */
